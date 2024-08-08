05e2efd400b38785974b86fb8539c2062b30eb1c
trhiep <hieptran.pa@gmail.com>
2024-08-08 16:17:19 +0700

    auto copy link to clipboard

--- get-link-coursera/assets/js/main.js
+++ get-link-coursera/assets/js/main.js
@@ -8,7 +8,22 @@ chrome.browserAction.onClicked.addListener(function (tab) {
         var postID = inputString.substring(0, tildeIndex);
         var updatedUrl = currentUrl.replace(/submit$/, "review");
         updatedUrl += "/" + postID;
-        alert(updatedUrl);
+
+        // Tạo một thẻ input tạm thời để lưu giá trị updatedUrl
+        var tempInput = document.createElement("input");
+        tempInput.value = updatedUrl;
+        document.body.appendChild(tempInput);
+
+        // Chọn và sao chép giá trị trong thẻ input tạm thời
+        tempInput.select();
+        tempInput.setSelectionRange(0, 99999); // Đối với thiết bị di động
+        document.execCommand("copy");
+
+        // Xóa thẻ input tạm thời sau khi sao chép
+        document.body.removeChild(tempInput);
+
+        // Thông báo cho người dùng rằng URL đã được sao chép
+        alert("Đã lưu link vào clipboard: " + updatedUrl);
       `
     }, function (results) {
         if (chrome.runtime.lastError) {
